940770135d1c9c9c0e09f4cd02c744daf09141b5
Wagner Faria do Amaral <wagnerfaria1601@gmail.com>
2026-05-19 15:07:11 -0400

    Altera o padrao do tamanho dos arquivos

--- backend-strapi/config/plugins.ts
+++ backend-strapi/config/plugins.ts
@@ -1,5 +1,11 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  upload: {
+    config: {
+      sizeLimit: 550 * 1024 * 1024,
+    },
+  },
+});
 
 export default config;
--- backend-strapi/src/index.ts
+++ backend-strapi/src/index.ts
@@ -1,4 +1,4 @@
-// import type { Core } from '@strapi/strapi';
+import type { Core } from '@strapi/strapi';
 
 export default {
   /**
@@ -7,7 +7,29 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+  register({ strapi }: { strapi: Core.Strapi }) {
+    strapi.db.lifecycles.subscribe({
+      models: ['plugin::upload.file'],
+      beforeCreate(event) {
+        const { mime, size } = event.params.data;
+
+        // Limites baseados no tipo de arquivo
+        if (mime.startsWith('image/')) {
+          if (size > 5 * 1024) {
+            throw new Error('O tamanho da imagem não pode exceder 5MB.');
+          }
+        } else if (mime.startsWith('video/')) {
+          if (size > 500 * 1024) {
+            throw new Error('O tamanho do vídeo não pode exceder 500MB.');
+          }
+        } else {
+          if (size > 50 * 1024) {
+            throw new Error('O tamanho do documento não pode exceder 50MB.');
+          }
+        }
+      },
+    });
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
